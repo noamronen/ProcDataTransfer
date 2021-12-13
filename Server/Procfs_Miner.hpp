2db171499c746ef13528e8cf13316c64f90381bb
noamronen <42314592+noamronen@users.noreply.github.com>
2021-12-13 02:32:25 +0200

    Create Procfs_Miner.hpp

---
+++ Server/Procfs_Miner.hpp
@@ -0,0 +1,44 @@
+//
+// Created by noam on 06/12/2021.
+//
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+struct data_type
+{
+    data_type();
+    data_type(int i,string description);
+    int position;
+    string description;
+};
+
+struct proc_type
+{
+    proc_type();
+    proc_type(string type,vector<data_type>);
+    string type;
+    vector<data_type> vec;
+};
+
+
+
+class Procfs_Miner
+{
+private:
+    vector<proc_type> procfs_options;//will contain a vector for each /proc/type
+public:
+    Procfs_Miner();
+    Procfs_Miner(vector<proc_type> procfs_options);
+    void setProcfs_options( vector<proc_type> procfs_options);
+    vector<proc_type> getProcfs_options();
+    //void addProcType(proc_type newOption);
+    void Mine_proc_stat_cpuDataPrint();
+    string  Mine_proc_stat_cpuDataStr();
+    void Mine_proc_memoryInfo();
+    void Mine_proc_KernelModule();
+};
